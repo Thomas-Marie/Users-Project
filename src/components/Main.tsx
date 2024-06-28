ad654d437bb3a9b2182779875b9e7b64d622568e
Thomas Marie <thomus97@gmail.com>
2024-06-28 16:27:39 +0200

    fix: main.tsx handle empty users fetch

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -55,17 +55,21 @@ const Main: React.FC = () => {
       <section className='w-1/2 p-4'>
         <h2 className='text-xl font-bold mb-4'>Utilisateurs GitHub</h2>
         <ul className='space-y-4'>
-          {users.map((user) => (
-            <li
-              key={user.login}
-              onClick={() => handleUserClick(user)}
-              className={`p-4 border rounded shadow cursor-pointer hover:bg-gray-100 ${selectedUser?.login === user.login ? "bg-gray-100" : ""}`}>
-              <div className='flex items-center space-x-4'>
-                <img src={user.avatar_url} alt={`Profil de ${user.login}`} className='w-12 h-12 rounded-full' />
-                <span>{user.login}</span>
-              </div>
-            </li>
-          ))}
+          {users ? (
+            users.map((user) => (
+              <li
+                key={user.login}
+                onClick={() => handleUserClick(user)}
+                className={`p-4 border rounded shadow cursor-pointer hover:bg-gray-100 ${selectedUser?.login === user.login ? "bg-gray-100" : ""}`}>
+                <div className='flex items-center space-x-4'>
+                  <img src={user.avatar_url} alt={`Profil de ${user.login}`} className='w-12 h-12 rounded-full' />
+                  <span>{user.login}</span>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li>Loading...</li>
+          )}
         </ul>
       </section>
       <section className='w-1/2 p-4 border-l'>
